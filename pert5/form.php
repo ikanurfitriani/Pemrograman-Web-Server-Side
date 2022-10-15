86691465a29f8c4895276c5ae65f33a74fdfbd32
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 16:59:08 +0700

    Delete pert5 directory

--- pert5/form.php
+++
@@ -1,8 +0,0 @@
-<form name="form" action="hasil.php" method="post">
-Kode Barang : <input type="text" name="kode" /><br/>
-Nama Barang : <input type="text" name="nama" /><br/>
-Harga : <input type="number" name="harga" /><br/>
-Jumlah Pembelian : <input type="number" name="jumlah" /><br/>
-
-<input type="submit" name="submit" value="SUBMIT">
-</form>
