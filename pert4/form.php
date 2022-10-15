5c8191253460dea62b83777ed37b8c39ba7bef5c
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 16:58:58 +0700

    Delete pert4 directory

--- pert4/form.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="tujuan.php">
-Nilai i: <input type="text" name="i"/><br/>
-Nilai num: <input type="text" name="num"/><br/>
-<input type="submit" value="SUBMIT" />
-</form>
