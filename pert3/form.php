9e2acec4e583cfecdf15302ef9d8560617e7a5bf
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 16:58:32 +0700

    Delete pert3 directory

--- pert3/form.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="tujuan.php">
-Nilai x: <input type="text" name="angkax"/><br/>
-Nilai y: <input type="text" name="angkay"/><br/>
-<input type="submit" value="SUBMIT" />
-</form>
